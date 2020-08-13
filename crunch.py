8256fddb3d4ef8568705b31fd1edf986f02bba4f
clicktoinstall <69070384+clicktoinstall@users.noreply.github.com>
2020-08-13 11:30:26 +0530

    Update crunch.py

--- crunch.py
+++ crunch.py
@@ -1,7 +1,6 @@
 #!usr/bin/evn python3
 import subprocess
 linux = input("what's your os , if you are using linux [sudo] and termux hit [enter] >>>")
-subprocess.call(linux + " apt install crunch ", shell=True)
 digit = input("digits >>> ")
 values = input("input >>> ")
 cd = input("directory >>> ")
